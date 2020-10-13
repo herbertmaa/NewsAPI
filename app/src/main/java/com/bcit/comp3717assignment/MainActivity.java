df04ac1f7fccdf8268ed03931e63f88e561f32d1
yongjukwon <michaelkyj@gmail.com>
2020-10-13 00:05:57 -0700

    initial commit

--- app/src/main/java/com/bcit/comp3717assignment/MainActivity.java
+++ app/src/main/java/com/bcit/comp3717assignment/MainActivity.java
@@ -2,13 +2,26 @@ package com.bcit.comp3717assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bitcoinBtn = findViewById(R.id.bitCoinBtn);
+        bitcoinBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        Intent intent = new Intent(this, ResultActivity.class);
+        startActivity(intent);
     }
 }
